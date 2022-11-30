ef56e3f95e7fe7892ce18b4408857d199bcffa5b
SaulSomir <113652224+SaulSomir@users.noreply.github.com>
2022-11-30 10:52:31 -0500

    Create database.js

---
+++ database.js
@@ -0,0 +1,20 @@
+onst mongoose = require('mongoose');
+const config = require('./config');
+
+const URI = config.mongodb.uri;
+
+const conectarDB = () => {
+    try {
+        mongoose.connect(URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+            .then(db => console.log('conectada'))
+            .catch(err => console.log(err));
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+};
+
+module.exports = conectarDB();
